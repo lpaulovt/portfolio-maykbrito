8705fb549c6dafa5fffd0977172610003254e157
Paulo Lopes <silvavitorlopes711@gmail.com>
2020-05-14 15:38:06 -0300

    fix: data.json

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
-const videos = require('./data')
+const data = require('./data.json')
 
 server.use(express.static('public'))
 
@@ -16,29 +16,16 @@ nunjucks.configure("views", {
 
 server.get("/", function(req, res){
 
-    const about = {
-        avatar: "https://avatars2.githubusercontent.com/u/6643122?s=400&u=1e9e1f04b76fb5374e6a041f5e41dce83f3b5d92&v=4",
-        name: "Mayk Brito",
-        role: "Instrutor - Rocketseat",
-        description: 'Programador full-stack, focado em trazer o melhor ensino para iniciantes em programação. Colaborador da <a href="https://rocketseat.com.br/" target="_blank">Rocketseat</a>',
-        links: [
-            {name: "Github", url: "http://github.com/maykbrito"},
-            {name: "Twitter", url: "http://twitter.com/maykbrito"},
-            {name: "Linkedin", url: "http://www.linkedin.com/in/maykbrito/"}
-        ]
-
-    }
-
-    return res.render("about", {about})
+    return res.render("about", {about: data.about})
 })
 
 server.get("/portfolio", function(req, res){
-    return res.render("portfolio", {items : videos})
+    return res.render("portfolio", {items : data.videos})
 })
 
 server.get("/video", function(req, res){
     const id = req.query.id
-    const video = videos.find(function(video){
+    const video = data.videos.find(function(video){
         if (video.id == id) {
             return true
         }
